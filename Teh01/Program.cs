eed204bfe754900a28f94c6362cf7a8e8fc5a2e7
K2898 <K2898@student.jamk.fi>
2017-01-19 10:28:27 +0200

    Teh01 ready

--- Teh01/Program.cs
+++ Teh01/Program.cs
@@ -10,6 +10,21 @@ namespace Teh01
     {
         static void Main(string[] args)
         {
+            int number;
+            Console.Write("Anna numero >");
+            string line = Console.ReadLine();
+            bool result = int.TryParse(line, out number);
+            if (result == true)
+            {
+                switch (number)
+                {
+                    case 1: Console.WriteLine("Yksi"); break;
+                    case 2: Console.WriteLine("Kaksi"); break;
+                    case 3: Console.WriteLine("Kolme"); break;
+                    default: Console.WriteLine("Joku muu luku"); break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
