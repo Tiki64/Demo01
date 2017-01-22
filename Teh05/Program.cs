3fbe2e874037fa4ebbb517bbc58846e4c7c3469f
Tiki <t.tikk@hotmail.com>
2017-01-22 14:00:57 +0200

    Valmis t6

--- Teh05/Program.cs
+++ Teh05/Program.cs
@@ -10,6 +10,19 @@ namespace Teh05
     {
         static void Main(string[] args)
         {
+            int total, seconds, hours, minutes;
+
+
+
+            Console.WriteLine(" Syota sekunnit numerona ");
+            total = int.Parse(Console.ReadLine());
+            minutes = total / 60;
+            seconds = total % 60;
+            hours = minutes / 60;
+            minutes = minutes % 60;
+
+            Console.WriteLine(total + " sekunttia on " + hours + " tuntia " + minutes + " minuuttia " + seconds +  " sekuntia ");
+
 
         }
 
