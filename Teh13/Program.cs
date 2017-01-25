0e8d189e87c5e73ef323e29ee97056ae9a18cfab
Tiki <t.tikk@hotmail.com>
2017-01-25 21:59:39 +0200

    Mada Mada!

--- Teh13/Program.cs
+++ Teh13/Program.cs
@@ -10,6 +10,21 @@ namespace Teh13
     {
         static void Main(string[] args)
         {
+            int[] points = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Anna pisteet ");
+                points[i] = int.Parse(Console.ReadLine());
+            }
+            int smallest_number = 999999999;
+            int highest_number = 0;
+            foreach (int value in points)
+            {
+                smallest_number = Math.Min(smallest_number, value);
+                highest_number = Math.Max(highest_number, value);
+            }
+            int sum = points.Sum() - (smallest_number + highest_number);
+            Console.WriteLine("Kokonaispisteet ovat " + sum);
         }
     }
 }
