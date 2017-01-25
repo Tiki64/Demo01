5d034e3fef83eedcbe0d4218885f7df9e95d7ecd
Tiki <t.tikk@hotmail.com>
2017-01-26 00:11:12 +0200

    You are only human.

---
+++ Teh15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teh15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna luku > ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number - 2; i++)
+            {
+                Random rand = new Random();
+                int luku = rand.Next(0, 101);
+            }
+        }
+    }
+}
